146993c73d7c40cfa4fbe28d00a29aa88b8df238
warren <warren@rocketeer>
2014-09-02 19:01:57 -0400

    Sync to the web i2c changes

--- arm_server_rpc.js
+++ arm_server_rpc.js
@@ -1,5 +1,5 @@
 /*
- * arm_server.js
+ * arm_server_rpc.js
  * JSON.RPC and key interface for the RobotArm controller.
  *
  * Creates a json.rpc server and waits for device action commands.
@@ -10,32 +10,17 @@
 
 var jsonServerPort = 3030;
 
-var keypress = require('keypress');
-var jayson = require('jayson');
 var util = require('util');
-var robotarm = require('./robotarm.js');
+var jayson = require('jayson');
+
+var keypress = require('keypress');
+var robotarm = require('./robotarm/robotarm.js');
 
 var arm1 = new robotarm.RobotArm(0x20);
 var arm2 = new robotarm.RobotArm(0x21);
-var arm = arm1;
+var arm = 'arm1.';
 
 var server = jayson.server();
-
-
-function testKeypress() {
-    /// Used for testing only
-    console.log('Waiting for keypress...');
-
-    keypress(process.stdin);
-    process.stdin.setRawMode(true);
-    process.stdin.on('keypress', function (ch, key) {
-        console.log('got "keypress"', key);
-        if (key && key.ctrl && key.name == 'c') {
-            process.stdin.pause();
-        }
-    });
-    process.stdin.resume();
-}
 
 function processKeys(ch, key) {
     console.log('keypress ch=' + (ch? ch : 'unkn'),
@@ -47,8 +32,7 @@ function processKeys(ch, key) {
             arm2.reset();
             process.stdin.pause();
         } else if (key.name == 'escape') {
-            arm1.reset();
-            arm2.reset();
+            stopAllMotors();
             process.exit();
         }
     }
@@ -57,21 +41,25 @@ function processKeys(ch, key) {
         return;
 
     switch (ch) {
-        case '1': arm = arm1; break;
-        case '2': arm = arm2; break;
+        case '1': arm = 'arm1.'; break;
+        case '2': arm = 'arm2.'; break;
 
-        case 'q': arm.m1.up(); break;
-        case 'a': arm.m1.down(); break;
-        case 'w': arm.m2.up(); break;
-        case 's': arm.m2.down(); break;
-        case 'e': arm.m3.up(); break;
-        case 'd': arm.m3.down(); break;
-        case 'r': arm.m4.up(); break;
-        case 'f': arm.m4.down(); break;
-        case 'z': arm.m5.up(); break;
-        case 'x': arm.m5.down(); break;
-        
-        case ' ': arm1.stopMotors(); arm2.stopMotors(); break;
+        case 'q': motorAction(arm + 'm1', 1); break;
+        case 'a': motorAction(arm + 'm1', 2); break;
+        case 'w': motorAction(arm + 'm2', 1); break;
+        case 's': motorAction(arm + 'm2', 2); break;
+        case 'e': motorAction(arm + 'm3', 1); break;
+        case 'd': motorAction(arm + 'm3', 2); break;
+        case 'r': motorAction(arm + 'm4', 1); break;
+        case 'f': motorAction(arm + 'm4', 2); break;
+        case 'z': motorAction(arm + 'm5', 1); break;
+        case 'x': motorAction(arm + 'm5', 2); break;
+        case 't': armAction(arm + 'led1', 1); break;
+        case 'g': armAction(arm + 'led1', 0); break;
+
+        case ' ':
+            stopAllMotors();
+            break;
     }
 }
 
@@ -82,21 +70,25 @@ function setupKeys() {
     process.stdin.resume();
 }
 
+function stopAllMotors() {
+    arm1.reset();
+    arm2.reset();
+}
+
+function armAction(device, action) {
+    var cmd = device + ".set(" + action + ")";
+    util.log("  armAction: " + cmd);
+    eval(cmd);
+}
+
 function motorAction(device, action) {
     var cmd = device + ".move(" + action + ")";
     console.log("  motorAction: " + cmd);
     eval(cmd);
 } 
 
-function setup() {
-    setupKeys();
-}
-
-setup();
-//testKeypress();
-
 server.tcp().listen(jsonServerPort, function () {
-    util.log('JSon server is listening on *: ' + jsonServerPort);
+    util.log('Json server ready on port ' + jsonServerPort);
 });
 
 server.method('doAction', function (device, action, callback) {
@@ -104,10 +96,14 @@ server.method('doAction', function (device, action, callback) {
 
     if ((/^arm[12].m[1-5]$/).test(device) && action >= 0 && action <= 2) {
         motorAction(device, action)
+    } else if ((/^arm[12].led[1]$/).test(device) && action >= 0 && action <= 1) {
+        armAction(device, action);
     } else {
         console.log("  invalid action: " + device + " - " + action);
     }
 });
+
+setupKeys();
 
 util.log('Waiting for keypress and json.rpc calls...');
 util.log('Use 1 for Arm 1, 2 for Arm 2');
